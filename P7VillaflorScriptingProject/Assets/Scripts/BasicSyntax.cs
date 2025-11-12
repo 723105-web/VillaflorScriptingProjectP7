3a11dfe1396e1818a58b8740a08b18d4da3d322c
D1luffy <723105@student.dusd.net>
2025-11-12 14:23:33 -0800

    Conventions and syntax
    I fixed my code so it could be more smooth.

--- P7VillaflorScriptingProject/Assets/Scripts/BasicSyntax.cs
+++ P7VillaflorScriptingProject/Assets/Scripts/BasicSyntax.cs
@@ -12,7 +12,7 @@ public class BasicSyntax : MonoBehaviour
         /*Hi there! 
          * this is two lines!
          * */
-        Debug.Log(transform.position.y < 5f);
+        Debug.Log(transform.position.x);
         if (transform.position.y <= 5f)
         {
             Debug.Log("I'm about to hit the ground!");
